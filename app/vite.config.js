e019efece0d02d57749c07265fce61ab9527958c
JesusArotinco <2411080011@undc.edu.pe>
2025-11-15 23:44:42 -0500

    version estable

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Proyecto_React_Vite/'  // Asegura que los assets funcionen en GitHub Pages
+  base: '/Proyecto_React_Vite/' // Muy importante para GitHub Pages
 })
