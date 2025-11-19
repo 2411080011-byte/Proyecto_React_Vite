5401857f0b21ab6660c79e3d865a5c8e6c32beff
JesusArotinco <2411080011@undc.edu.pe>
2025-11-19 09:41:10 -0500

    version estable: cambio de nombre de los componentes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,13 +4,13 @@ import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import VideoBackground from "./components/VideoBackground";
+import ImageBackground from "./components/ImageBackground";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div className="relative min-h-screen text-gray-100 font-sans overflow-hidden">
-      <VideoBackground />
+      <ImageBackground />
       <div className="relative z-10">
         <Header />
         <Hero />
